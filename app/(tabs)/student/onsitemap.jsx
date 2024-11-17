6f62fe5e3defd3acedeacbc51c68001027f9e2a0
marparc <122086810+marparc@users.noreply.github.com>
2024-11-17 12:07:27 +0800

    View Map Added
    Added Maps feature, gets user location.

--- app/(tabs)/student/onsitemap.jsx
+++ app/(tabs)/student/onsitemap.jsx
@@ -1,12 +1,61 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
-const Map = () => {
+export default function JobMap() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      // Request permission to access location
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      // Get current position
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    };
+
+    getLocation();
+  }, []);
+
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      {location ? (
+        <MapView
+          style={styles.map}
+          region={location}
+          showsUserLocation={true}
+          loadingEnabled={true}
+        >
+          {/* Marker at the current location */}
+          <Marker coordinate={location} title="You are here" />
+        </MapView>
+      ) : (
+        <Text>{errorMsg || "Getting your location..."}</Text>
+      )}
     </View>
   );
-};
+}
 
-export default Map;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
